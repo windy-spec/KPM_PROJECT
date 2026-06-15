585113838555c9f323fd3a06d828636967428ac9
ĐỖ THANH PHONG <phongdz072@gmail.com>
2026-06-15 17:15:16 +0700

    git push and clean code

--- backend/nodejs/tmp_cat.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  const categories = await prisma.product_categories.findMany({ where: { parent_id: null } });
-  console.log(categories.map(c => c.category_name));
-}
-run().finally(() => prisma.$disconnect());
